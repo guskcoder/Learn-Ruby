4fe0a5f4021110dfee85b4d724b0f1f6896eaca3
guskcoder <gustavo@imaginative.company>
2021-07-20 09:56:48 -0300

    Branching add

---
+++ branching.rb
@@ -0,0 +1,10 @@
+puts "Hello, what's your name?"
+name = gets.chomp
+puts "Hello, #{name}."
+
+if name == "Chris"
+  puts "What a lovely name!"
+else
+  puts "Your future is...oh my! Look at the time!"
+  puts "I really have to go. Sorry!"
+end
--- comparison.rb
+++ comparison.rb
@@ -3,3 +3,6 @@ puts 1 < 2
 
 puts 5 >= 5 
 puts 5 <= 4
+
+puts "a giant woman" < "Watermelon Steven"
+puts "a giant woman".downcase < "Watermelon Steven".downcase
