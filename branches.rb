b9046dc15617194c55476756c0c7cf69001b0315
guskcoder <gustavo@imaginative.company>
2021-07-22 09:59:52 -0300

    Branche Add

---
+++ branches.rb
@@ -0,0 +1,14 @@
+puts "Hello, and welcome to seventh grade English."
+puts "My name is Mrs. Gabbard. And your name is...?"
+name = gets.chomp
+
+if name == name.capitalize
+  puts "Please take a seat, #{name}."
+else
+  puts "#{name}? You mean #{name.capitalize}, right?" puts "Don't you even know how to spell your name??" reply = gets.chomp
+  if reply.downcase == "yes"
+    puts "Hmmph! Well, sit down!"
+  else
+    15 puts "GET OUT!!"
+  end
+end
