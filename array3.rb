15e2ac4e53d569a6e38936f616a32ee8cc517c9f
Gustavo Henrique <gustavo@imaginative.company>
2021-08-14 11:37:16 -0300

    Array 3 Add

---
+++ array3.rb
@@ -0,0 +1,8 @@
+languages = ["English", "Norwgian", "Ruby"]
+
+languages.each do |lang|
+  puts "I love #{lang}! Don't you?"
+end
+
+puts "And let's hear it for Fortran!"
+puts "<crickets chirp in the distance>"
