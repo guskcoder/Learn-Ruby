4d5ddcd7bb689013f703f97c8f9d78b1875dd8c9
Gustavo Henrique <gustavo@imaginative.company>
2021-08-16 19:21:03 -0300

    Array Method Add

---
+++ array6.rb
@@ -0,0 +1,11 @@
+favorites = []
+favorites.push("raindrops on roses")
+favorites.push("whiskey on kittens")
+
+puts favorites[0]
+puts favorites.last
+puts favorites.lenght
+
+puts favorites.pop
+puts favorites
+puts favorites.lenght
