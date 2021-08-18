2139acc2fa6fe1a005073d42cd41be09f7f3f4d0
Gustavo Henrique <gustavo@imaginative.company>
2021-08-18 12:59:14 -0300

    Methods Game 2

--- methods.rb
+++ methods.rb
@@ -1,6 +1,6 @@
 match_1 = ["vanilla", "chocolate"]
 match_2 = ["rhubarb", "pistachio"]
-macth_3 = []
+match_3 = []
 winner = nil
 
 puts "Welcome to ULTIMATE FLAVOR TOURNAMENT!"
@@ -9,7 +9,7 @@ puts "MATCH 1: Which Flavor is best?"
 puts "0. "+match_1[0]
 puts "1. "+match_1[1]
 while true
-  aswer = gets.chomp.downcase
+  answer = gets.chomp.downcase
   if (answer == "0" || answer == "1")
     match_3[0] = match_1[answer.to_i]
     break
@@ -25,7 +25,7 @@ puts "1. "+match_2[1]
 while true
   answer = gets.chomp.downcase
   if (answer == "0" || answer == "1")
-    mastch_3[1] = match_2[answer.to_i]
+    match_3[1] = match_2[answer.to_i]
     break
   else
     puts "Please answer '0' or '1'."
