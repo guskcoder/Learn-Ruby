6c931f57bce48b51d8724a1d8388b5e0317be67f
Gustavo <gustavo@imaginative.company>
2021-09-23 23:57:34 -0300

    Condicional Add

---
+++ Ruby-OneBitCode/Estrutura de Controle/else.rb
@@ -0,0 +1,9 @@
+day = 'Sunday'
+
+if day == 'Sunday'
+  lunch = 'Special'
+else
+  lunch = 'Normal'
+end
+
+puts "Lunch is #{lunch} today"
---
+++ Ruby-OneBitCode/Estrutura de Controle/if.rb
@@ -0,0 +1,8 @@
+day = 'Sunday'
+lunch = 'Normal'
+
+if day == 'Sunday'
+  lunch = 'Special'
+end
+
+puts "Lunch is #{lunch} today"
