b461054895bfe239dc51af4f049718430ac24058
Gustavo <gustavo@imaginative.company>
2021-09-23 23:06:36 -0300

    Ruby New Op Add 3

--- Ruby-OneBitCode/entrada_saida.rb
+++ Ruby-OneBitCode/entrada_saida.rb
@@ -1,14 +1,7 @@
 print 'Digite seu nome: '
 name = gets.chomp
 
-<<<<<<< HEAD
 print 'Digite seu Sobrenome: '
 sobrenome = gets.chomp
 
 puts "Hello, #{name} #{sobrenome}!"
-=======
-print 'Digite seu sobrenome: '
-lastname = gets.chomp
-
-puts "Hello, #{name} #{lastname}!"
->>>>>>> 3bbffd7b212de34114943a1b36c1d3c699b9feed
---
+++ Ruby-OneBitCode/operacoes.rb
@@ -0,0 +1,12 @@
+puts 'Soma entre dois números'
+print 'Digite o primeiro número inteiro: '
+
+number1 = gets.chomp.to_i
+
+print 'Digite o segundo número inteiro: '
+
+number2 = gets.chomp.to_i
+
+addition = number1 + number2
+
+puts "O Resultado da adição entre os números é #{addition}"
