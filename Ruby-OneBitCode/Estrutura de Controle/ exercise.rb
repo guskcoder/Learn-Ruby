c09e88545089dc1cb1d423b020b5a619850d2e1e
Gustavo <gustavo@imaginative.company>
2021-11-05 08:30:34 -0300

    Exercice Soma 2

--- Ruby-OneBitCode/Estrutura de Controle/ exercise.rb
+++ Ruby-OneBitCode/Estrutura de Controle/ exercise.rb
@@ -16,17 +16,17 @@ loop do
     print 'Digite o Primeiro Número: '
     number1 = gets.chomp.to_i
 
-    rint 'Digite o Segundo Número: '
+    print 'Digite o Segundo Número: '
     number2 = gets.chomp.to_i
     case option
     when 1
-    result = "#{number1} + #{number2} = #{number1} + #{number2}"
+    result = "#{number1} + #{number2} = #{number1 + number2}"
     when 2
-      result = "#{number1} - #{number2} = #{number1} - #{number2}"
+      result = "#{number1} - #{number2} = #{number1 - number2}"
     when 3
-      result = "#{number1} * #{number2} = #{number1} * #{number2}"
+      result = "#{number1} * #{number2} = #{number1 * number2}"
     when 4
-      result = "#{number1} / #{number2} = #{number1} / #{number2}"
+      result = "#{number1} / #{number2} = #{number1 / number2}"
     end
     when 0
       break
@@ -35,19 +35,3 @@ loop do
     end
     system "clear"
   end
-
-  if option == 1
-    print 'Digite o ano de nascimento: '
-    year_of_birth = gets.chomp.to_i
-    print 'Digite o ano atual: '
-    current_year = gets.chomp.to_i
-    age = current_year - year_of_birth
-    result = "Quem nasceu no ano de #{year_of_birth}, tem #{age} anos em #{current_year}"
-  elsif option == 0
-    break
-  else
-    result = 'Opção Inválida'
-  end
-
-  system "clear"
-end
